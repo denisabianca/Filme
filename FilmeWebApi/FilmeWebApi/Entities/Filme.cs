e806ecd13167a1d58ae54a119529ac94e700d9a8
denisabianca <denisa.bianca19@yahoo.com>
2020-07-14 02:02:52 +0300

    Entities

---
+++ FilmeWebApi/FilmeWebApi/Entities/Filme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace FilmeWebApi.Entities
+{
+    public class Filme
+    {
+        [Key] 
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Titlu  { get; set; }
+
+        [MaxLength(2500)]
+        public string Descrierea { get; set; }
+
+        [MaxLength(150)]
+        public int AnulAparitiei { get; set; }
+
+        [Required]
+        public Guid RegizorId { get; set; }
+
+        [ForeignKey("Regizor")]
+        public virtual Regizor Regizor { get; set; }
+
+        public bool? Deleted { get; set; }
+
+    }
+}
---
+++ FilmeWebApi/FilmeWebApi/Entities/Regizor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeWebApi.Entities
+{
+    public class Regizor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Nume { get; set; }
+
+        [MaxLength(150)]
+        public string Prenume { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+}
